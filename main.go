6aa156955d236f3e49bfe0e94719a5c85a426c6a
Yunus Gök <ynsmrgok@gmail.com>
2022-03-28 20:34:27 +0300

    gin server and swagger added

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"picnshop/pkg/graceful"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
 func main() {
+	r := gin.Default()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+	graceful.ShutdownGin(srv, time.Second*5)
 
 }
