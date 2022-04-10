076f791271e2e91fa7a8f4aaececeb762775cca2
Yunus Gök <ynsmrgok@gmail.com>
2022-04-10 16:46:51 +0300

    validation errors added

--- internal/domain/user/errors.go
+++ internal/domain/user/errors.go
@@ -7,4 +7,8 @@ import (
 var (
 	ErrUserExistWithName = errors.New("User already exist with same username in database")
 	ErrUserNotFound      = errors.New("User not found")
+	//TODO: Validation error can be specified
+	ErrMismatchedPasswords = errors.New("Given passwords does not match")
+	ErrInvalidUsername     = errors.New("Invalid username")
+	ErrInvalidPassword     = errors.New("Invalid password")
 )
