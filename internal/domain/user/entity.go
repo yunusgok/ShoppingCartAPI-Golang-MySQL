01422cbd956963a82f21c0e50e53536919f0b14a
Yunus Gök <ynsmrgok@gmail.com>
2022-04-10 16:46:05 +0300

    password2 field on constructor added

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -8,17 +8,19 @@ type User struct {
 	gorm.Model
 	Username  string `gorm:"type:varchar(30)"`
 	Password  string `gorm:"type:varchar(100)"`
+	Password2 string `gorm:"-"`
 	Salt      string `gorm:"type:varchar(100)"`
 	Token     string `gorm:"type:varchar(100)"`
 	IsDeleted bool
 	IsAdmin   bool
 }
 
-func NewUser(username string, password string) *User {
+func NewUser(username, password, password2 string) *User {
 
 	return &User{
 		Username:  username,
 		Password:  password,
+		Password2: password2,
 		IsDeleted: false,
 		IsAdmin:   false,
 	}
